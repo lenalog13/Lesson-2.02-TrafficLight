23f4eaafeeb7b0eb8f6b6cb6e62135f37e4a48db
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2022-12-14 16:40:10 +0300

    logic implementation

--- Lesson 2.02 TrafficLight/ViewController.swift
+++ Lesson 2.02 TrafficLight/ViewController.swift
@@ -9,18 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+        
     @IBOutlet weak var redView: UIView!
-    
     @IBOutlet weak var yellowView: UIView!
-    
     @IBOutlet weak var greenView: UIView!
     
+    @IBOutlet weak var switchButton: UIButton!
+    
+    private let filletSize: CGFloat = 75
+    
+    private var counter = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = filletSize
+        yellowView.layer.cornerRadius = filletSize
+        greenView.layer.cornerRadius = filletSize
     }
 
-
+    @IBAction func buttinDidTapped(_ sender: Any) {
+        switchButton.setTitle("NEXT", for: .normal)
+        switch counter {
+        case 1:
+            redView.alpha = 1
+            greenView.alpha = 0.3
+            counter += 1
+        case 2:
+            yellowView.alpha = 1
+            redView.alpha = 0.3
+            counter += 1
+        case 3:
+            greenView.alpha = 1
+            yellowView.alpha = 0.3
+            counter = 1
+        default:
+            break
+        }
+        
+    }
+    
 }
 
