be5b1cf96059a0e4216449aec2c97fd7a0d8a12a
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2022-12-14 14:36:23 +0300

    Initial Commit

---
+++ Lesson 2.02 TrafficLight/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Lesson 2.02 TrafficLight
+//
+//  Created by Елена Логинова on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redView: UIView!
+    
+    @IBOutlet weak var yellowView: UIView!
+    
+    @IBOutlet weak var greenView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
